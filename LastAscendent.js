cb92ffc6c4ed0dc0c459fba2fdc20f7de140cfbe
Ricardo Costa <ricardocosta101085@gmail.com>
2017-07-01 16:25:21 +0100

    last ascendent update

--- LastAscendent.js
+++ LastAscendent.js
@@ -18,3 +18,34 @@ function findOldestAncester(ancesterName, name, degree, father){
 
 console.log(findOldestAncester("?","Carolus Haverbeke",0,1));
 console.log(findOldestAncester("?","Carolus Haverbeke",0,0));
+
+function findOldestAncester2(ancesterName, name, degree, sex){
+  //console.log(ancesterName + " " + name + " " + degree + " " + sex);
+  var ancester = ancestries.find(function(element){
+    return element.name == (ancesterName == "?" ? name : ancesterName)
+  })
+  console.log(
+    (ancester ? "name: " + ancester.name + " father: " + ancester.father : "---")
+     + " | " +
+    (ancester ? "name: " + ancester.name + " mother: " + ancester.mother : "---")
+  );
+  if (ancester) {
+    ++degree;
+    return findOldestAncester2(
+      ancester.mother,
+      ancester.name,
+      degree,
+      "lado 1"
+    ) && findOldestAncester2(
+      ancester.father,
+      ancester.name,
+      degree,
+      "lado 2"
+    )}
+  else {
+    ++degree
+    return ancesterName + " " + degree + "º"
+  }
+}
+
+console.log(findOldestAncester2("?","Carolus Haverbeke",0,""));
