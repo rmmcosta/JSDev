67a34a322e5b8197a8a091479be3483edf2f1715
rmmcosta <ricardocosta101085@gmail.com>
2017-07-05 21:13:19 +0100

    Tlm

---
+++ bindFunction2.js
@@ -0,0 +1,15 @@
+function LateBloomer() {
+  this.petalCount = Math.ceil(Math.random() * 12) + 1;
+}
+
+LateBloomer.prototype.bloom = function() {
+  window.setTimeout(this.declare.bind(this), 1000);
+};
+
+LateBloomer.prototype.declare = function() {
+  console.log('I am a beautiful flower with ' +
+    this.petalCount + ' petals!');
+};
+
+var flower = new LateBloomer();
+flower.bloom();  
